c2d0376dbdbb90a416d238a370119e93ee7dbe53
kage tris <phumanong@gmail.com>
2019-08-24 18:52:32 +0700

    add api post

--- model/mongo/index.js
+++ model/mongo/index.js
@@ -1,7 +1,8 @@
 const Models = {
-  User: require('./user'),
-  Category: require('./category'),
-  CategoryPost: require('./categoryPost')
+    User: require('./user'),
+    Category: require('./category'),
+    CategoryPost: require('./categoryPost'),
+    Post: require('./post')
 }
 
 module.exports = Models
