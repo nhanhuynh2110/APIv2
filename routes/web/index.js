421e62084c5a3f46e5fa68483420947409bb89de
nhanhuynh2110 <trungnhan21102110@gmail.com>
2019-08-27 15:08:30 +0700

    add api web

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,15 +1,6 @@
 var express = require('express')
 var router = express.Router()
-var jwt = require('jsonwebtoken')
 
-require('./category')(router)
-require('./blog')(router)
-require('./home')(router)
-require('./video')(router)
-require('./product')(router)
-require('./homecontent')(router)
-require('./header')(router)
-require('./banner')(router)
-
+require('./edu')(router)
 
 module.exports = router
