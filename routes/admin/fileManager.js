8773cd06529d90f11644798b8aa866fbe0e98f36
phumanong2311 <phumanong@gmail.com>
2019-10-11 22:36:09 -0700

    update gallery

--- routes/admin/fileManager.js
+++ routes/admin/fileManager.js
@@ -1,30 +1,64 @@
 const async = require('async')
 const ObjectId = require('mongoose').Types.ObjectId
 const fs = require('fs')
+var rimraf = require('rimraf')
 const path = require('path')
 
 const authUser = require('../../controller/authenticate/autuser')
 const utility = require('../../helper/utility')
-const rootPath = './uploads/file-manager/'
+const rootPath = './uploads/file-manager'
+
+const routePrefix = '/file-manager'
 
 module.exports = function (router) {
-
-  router.post('/file-manager', authUser.checkTokenAdmin, (req, res) => {
+  router.post(routePrefix, authUser.checkTokenAdmin, (req, res) => {
     try {
-      console.log(req.body)
       const {dirPath} = req.body
       if (!dirPath) return utility.apiResponse(res, 500, 'dirPath is invalid')
-      // if (fs.existsSync(rootPath + dirPath)) return utility.apiResponse(res, 500, 'folder-exits')
       if (!fs.existsSync(rootPath + dirPath)) {
-        fs.mkdir(rootPath + dirPath, (err) => {
+        fs.mkdir(rootPath + '/' + dirPath, (err) => {
+          
           if (err) return utility.apiResponse(res, 500, err.toString())
           return utility.apiResponse(res, 200, 'success', true)
         })
+      } else {
+        return utility.apiResponse(res, 500, 'create folder fail')
       }
-      return utility.apiResponse(res, 500, 'create folder fail')
-    } catch (error) { 
-      console.log('error', error)
-      utility.apiResponse(res, 500, error.toString(), null)
-    }
+    } catch (error) { return utility.apiResponse(res, 500, error.toString(), null) }
+  })
+
+  router.get(routePrefix, authUser.checkTokenAdmin, (req, res) => {
+    try {
+      const {dirPath} = req.query
+      const dir = dirPath ? rootPath + '/' + dirPath : rootPath
+      if (!fs.existsSync(dir)) return utility.apiResponse(res, 500, 'dirPath is invalid')
+      const folders = []
+      const files = []
+
+      fs.readdir(dir, (err, filesOrFolder) => {
+        if (err) return utility.apiResponse(res, 500, err.toString())
+        filesOrFolder.forEach(file => {
+          if (fs.lstatSync(`${dir}/${file}`).isDirectory()) folders.push({ name: file })
+          else files.push({ name: file })
+        })
+
+        return utility.apiResponse(res, 200, 'success', { folders, files })
+
+      })
+    } catch (error) { return utility.apiResponse(res, 500, error.toString(), null) }
+  })
+
+  router.delete(`${routePrefix}`, authUser.checkTokenAdmin, (req, res) => {
+    try {
+      const {path} = req.body
+      if (!path) return utility.apiResponse(res, 500, 'dirPath is invalid')
+      if (!fs.existsSync(rootPath + '/' + path)) {
+        return utility.apiResponse(res, 500, 'dirPath is invalid')
+      } else {
+        rimraf(rootPath + '/' + path, function (err, data) {
+          return utility.apiResponse(res, 200, 'Sucess', true)
+        })
+      }
+    } catch (error) { return utility.apiResponse(res, 500, error.toString()) } 
   })
 }
