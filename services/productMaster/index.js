657edcaffd9e089dd237b2c48cb175466b393ce6
nhanhuynh2110 <trungnhan21102110@gmail.com>
2019-10-28 17:16:35 +0700

    api product master

--- services/productMaster/index.js
+++ services/productMaster/index.js
@@ -1,42 +1,51 @@
 const _ = require('lodash')
 var ObjectId = require('mongoose').Types.ObjectId
-
 const utility = require('../../helper/utility')
-const {countDocument, excuteQuery, findOneId, save, update} = require('../../model/mongo/util')
+const {countDocument, excuteQuery, findOneId, create, update, deleteById, calSkip, formatSort} = require('../../model/mongo/util')
 const {ProductMaster, Category} = require('../../model/mongo')
 
-module.exports = () => {
-  return {
-    all: async ({ $search, sort, sortType, page, pageSize, isDelete }) => {
-      let query = {}
-      if ($search) query['$text'] = { $search }
-      if (sort) query[sort] = sortType && sortType === 'asc' ? 1 : -1
-      query.isDelete = isDelete === 'true'
-      const count = await countDocument(ProductMaster, query)
-      return excuteQuery(ProductMaster, query).then(list => ({ total: count, list }))
-    },
-
-    findId: (id) => findOneId(ProductMaster, id),
-
-    create: (payload) => {
-      return hadlePayloadForm(payload).then(data => save(ProductMaster, data))
-    },
-
-    update: (payload, id) => {
-      return hadlePayloadForm(payload).then(data => update(ProductMaster, { _id: ObjectId(id) }, data))
+module.exports = {
+  detail: async (id) => findOneId(ProductMaster, id),
+  create: (payload) => hadlePayloadForm(payload).then(resp => create(ProductMaster, resp)),
+  updateById: async (id, payload) => {
+    const product = await findOneId(ProductMaster, id)
+    if (!product) return Promise.resolve(null)
+    return hadlePayloadForm(payload).then(resp => update(ProductMaster, {_id: ObjectId(id)}, resp))
+  },
+  deleteById: async (id) => {
+    const product = await findOneId(ProductMaster, id)
+    if (!product) return Promise.resolve(null)
+    return deleteById(ProductMaster, id).then(() => product)
+  },
+  filter: ({$searchKey, isDelete, pageSize, pageNumber, colSort, typeSort}) => {
+    const query = {}
+    if ($searchKey) {
+      query['$text'] = { $search: $searchKey, $caseSensitive: true, $diacriticSensitive: true }
     }
-  }
+    query.isDelete = isDelete === 'true'
+    const tasks = []
+    tasks.push(() => countDocument(ProductMaster, query).then(count => count))
+    tasks.push(() => {
+      const skip = calSkip(pageSize, pageNumber)
+      const sort = formatSort(colSort, typeSort)
+      return excuteQuery(ProductMaster, query).skip(skip).limit(parseInt(pageSize)).sort(sort)
+    })
+    return utility.runParrallel(tasks).then(data => ({
+      total: data[0],
+      list: data[1]
+    }))
+  },
+  getProductActive: () => excuteQuery(ProductMaster, {isActive: true, isDelete: false})
 }
 
-const hadlePayloadForm = (payload) => {
+const hadlePayloadForm = async (payload) => {
   if (payload.title) payload.link = utility.formatLink(payload.title)
+  if (!payload.categoryId) return Promise.resolve(payload)
   const categoryId = _.get(payload, 'categoryId')
   if (!categoryId) return Promise.resolve(payload)
+  const cate = await findOneId(Category, categoryId)
 
-  Category.findOne({ _id: ObjectId(categoryId) }, (error, cat) => {
-    if (error) return Promise.reject(error)
-    if (!cat) return Promise.reject(new Error('category invalid'))
-    payload.categoryParentId = cat.parentId ? cat.parentId : categoryId
-    return Promise.resolve(payload)
-  })
+  if (!cate) return Promise.reject(new Error('category not exist!!!'))
+  payload.categoryParentId = cate.parentId ? cate.parentId : categoryId
+  return Promise.resolve(payload)
 }
